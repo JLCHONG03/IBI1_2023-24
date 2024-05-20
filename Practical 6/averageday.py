7eddeac52f5719d4e025abcde0e68d73074584d9
JLCHONG03 <bryanchong042003@gmail.com>
2024-05-20 16:06:35 +0800

    Commit week6 practical

--- Practical 6/averageday.py
+++ Practical 6/averageday.py
@@ -1,15 +1,30 @@
-#use matplotlib to plot graphs in python
+#import necessary libraries
 #store the data in dictionary
-#plot the figure in the dictionary
-
+#create an "other" variable and count the hour spent on it 
+#add the "other" variable into the dictionary
+#plot a pie chart for the data in the dictionary
 import matplotlib.pyplot as plt 
 
-Time_spent_during_an_average_day = [8,6,3.5,2,1,3.5]
-Activity = ['Sleeping','Classes','Studying','TV','Music','Others'] 
+Activity ={
+    'Sleeping': 8,
+    'Classes': 6,
+    'Studying': 3.5,
+    'TV': 2,
+    'Music': 1
+}
+
+Total_hours_in_a_day = 24
+Time_spent = sum(Activity.values())
+Others = Total_hours_in_a_day - Time_spent
+Activity['Others'] = Others
 
 plt.figure() 
-plt.pie(Time_spent_during_an_average_day, labels = Activity, startangle = 90)
+plt.pie(Activity.values(), labels=Activity.keys(), startangle = 90)
+plt.title("Average day of a univeristy student")
 plt.show()
 plt.clf()
+#print the number of hours spent on a specific activity 
+Interested_activity = 'Classes'
+print(f"Number of hours spent on {Interested_activity}: {Activity.get(Interested_activity, 0)}")
 
 
--- Practical 6/citysize.py
+++ Practical 6/citysize.py
@@ -1,7 +1,5 @@
 #import the necessary libraries
-#store the data in the dictionary
-#plot the figure in  the dictionary
-#use the subplot function to plot two bar chart in one figure
+#store the data
 import matplotlib.pyplot as plt 
 
 uk_cities = [0.56, 0.62, 0.04, 9.7]
@@ -9,8 +7,16 @@ uk_names = ["Edinburgh", "Glasglow", "Stirling", "London"]
 china_cities = [0.58, 8.4, 29.9, 22.2]
 china_names = ["Haining", "Hangzhou", "Shanghai", "Beijing"]
 
-plt.figure()
-plt.subplot(2,1,1)
+#sort the population sizes using the sorted function
+#print the sorted values
+sort_uk_cities = sorted(uk_cities)
+sort_china_cities = sorted(china_cities)
+print("Sorted values for the populations of cities in the UK:", sort_uk_cities)
+print("Sorted values for the populations of cities in China:", sort_china_cities)
+
+#plot the a bar chart for the data 
+plt.figure(figsize=(10,8))
+plt.subplot(2,1,1)#use the subplot function to plot two bar chart in one figure
 plt.bar(uk_names, uk_cities)
 plt.title("Population of UK cities in 2024")
 plt.xlabel("Cities in UK")
@@ -21,7 +27,7 @@ plt.bar(china_names, china_cities)
 plt.title("Population of China cities in 2024")
 plt.xlabel("Cities in china")
 plt.ylabel("Population (in million)")
-plt.tight_layout()
+plt.tight_layout()#adjust the spacing between two bar plots
 plt.show()
 plt.clf()
 
